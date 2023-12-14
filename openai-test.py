eeb68b5e4a80dc755774b79b5e0cfdb7ad03e28c
Tonge-Shim <tonge20@naver.com>
2023-12-14 15:57:38 +0900

    response_format added

--- openai-test.py
+++ openai-test.py
@@ -3,9 +3,12 @@ client = OpenAI()
 
 completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
+    # response_format={"type": "json_object" },
     messages=[
-        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}
     ]
     )
 
